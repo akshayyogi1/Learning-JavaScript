0986364985b82fca683d024dbf4c6815f71d60c0
akshayyogi1 <147067935+akshayyogi1@users.noreply.github.com>
2024-04-02 02:26:29 +0000

    Variables

---
+++ js-basics/js-Variable.js
@@ -0,0 +1,9 @@
+const accountId = 12334;
+let accountName= "Akshay";
+var accountEmail = "Akshay12@gmail.com"
+
+accountName="Aman";//  using let keyword we can update or change the value of variable
+// accountId = 41524;
+accountEmail = "aman123@gmail.com"
+
+console.log(accountId, accountName, accountEmail);
